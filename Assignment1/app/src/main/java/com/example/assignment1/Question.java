5dedd2bfe45d6113caceb526fe29f3847d81f579
Adrian-Luerssen <91116739+Adrian-Luerssen@users.noreply.github.com>
2022-02-12 20:41:50 +0100

    changing of project name

---
+++ Assignment1/app/src/main/java/com/example/assignment1/Question.java
@@ -0,0 +1,19 @@
+package com.example.assignment1;
+
+public class Question {
+    private String question;
+    private boolean answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public boolean isCorrect(boolean answer) {
+        return this.answer == answer;
+    }
+
+    public Question(String question, boolean answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
